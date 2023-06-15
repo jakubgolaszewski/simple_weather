d42175b43e39b4bd2c23460419e75d707f6d0725
Jakub Gołaszewski <jgolaszewski6@gmail.com>
2023-06-15 19:01:02 +0200

    Add API Request to repository

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,9 +1,18 @@
+import 'package:dio/dio.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
 class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    return const WeatherModel(city: 'Warsaw', temperature: -5.5);
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=6e84c9c1019e4231880195208231406&q=$city&aqi=no');
+    final responseData = response.data;
+    if (responseData == null) {
+      return null;
+    }
+    final name = responseData['location']['name'] as String;
+    final temperature = responseData['current']['temp_c'] as double;
+    return WeatherModel(city: name, temperature: temperature);
   }
 }
