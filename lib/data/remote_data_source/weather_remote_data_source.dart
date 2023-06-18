74fd04c0c3410967d8464ab3512abd9f043bd661
Jakub Gołaszewski <jgolaszewski6@gmail.com>
2023-06-18 09:58:32 +0200

    add handling dioerrors and blocconsumer

--- lib/data/remote_data_source/weather_remote_data_source.dart
+++ lib/data/remote_data_source/weather_remote_data_source.dart
@@ -4,8 +4,13 @@ class WeatherRemoteDataSource {
   Future<Map<String, dynamic>?> getWeatherData({
     required String city,
   }) async {
-    final response = await Dio().get<Map<String, dynamic>>(
-        'http://api.weatherapi.com/v1/current.json?key=6e84c9c1019e4231880195208231406&q=$city&aqi=no');
-    return response.data;
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(
+          'http://api.weatherapi.com/v1/current.json?key=6e84c9c1019e4231880195208231406&q=$city&aqi=no');
+      return response.data;
+    } on DioError catch (error) {
+      throw Exception(
+          error.response?.data['error']['message'] ?? 'Unknown error');
+    }
   }
 }
